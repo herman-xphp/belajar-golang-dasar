c0629dc6b66110339b51c1b637dd943820edd1cc
herman-xphp <hermanxphp@gmail.com>
2025-09-22 23:14:03 +0800

    add variable declaration and initialization example

---
+++ variable.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Herman"
+	fmt.Println(name)
+
+	name = "Irnawati"
+	fmt.Println(name)
+
+	var (
+		firstName = "Ucok"
+		lastName  = "Baba"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
