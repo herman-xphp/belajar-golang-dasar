dfbdf0735b473bc340eff41ae541a6d6faaa8f28
herman-xphp <hermanxphp@gmail.com>
2025-09-24 15:56:48 +0800

    add defer, panic and recover handling examples

---
+++ panic.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func endApp() {
+	fmt.Println("End app")
+	message := recover()
+	fmt.Println("Terjadi panic", message)
+}
+
+func runApp(error bool) {
+	defer endApp()
+	if error {
+		panic("Ups Error")
+	}
+}
+
+func main() {
+	runApp(true)
+	fmt.Println("Ucok Baba")
+}
