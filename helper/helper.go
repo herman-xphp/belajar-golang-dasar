ae1e4a4dc89fabafc489d96bb5025773146d6d1c
herman-xphp <hermanxphp@gmail.com>
2025-09-25 21:59:30 +0800

    access modifier

--- helper/helper.go
+++ helper/helper.go
@@ -1,5 +1,19 @@
 package helper
 
+import "fmt"
+
+var version = "1.0.0"
+var Application = "golang"
+
+func sayGoodBye(name string) string {
+	return "Good Bye " + name
+}
+
 func SayHello(name string) string {
 	return "Hello " + name
+}
+
+func contoh() {
+	sayGoodBye("Ucok")
+	fmt.Println(version)
 }
--- import.go
+++ import.go
@@ -8,4 +8,8 @@ import (
 func main() {
 	result := helper.SayHello("Ucok")
 	fmt.Println(result)
+
+	fmt.Println(helper.Application)
+	// fmt.Println(helper.version)	// tidak bisa diakses
+	// fmt.Println(helper.sayGoodBye("Ucok")) // tidak bisa diakses
 }
